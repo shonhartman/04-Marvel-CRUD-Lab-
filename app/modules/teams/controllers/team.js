1aff6bb6c03e53e4b735453de7c5c4713e542f0e
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-11 15:54:54 -0500

    ...

--- app/modules/teams/controllers/team.js
+++ app/modules/teams/controllers/team.js
@@ -23,6 +23,7 @@ class TeamController {
 	constructor($http, $stateParams) {
     this._$http = $http;
     this.id = $stateParams.id;
+		this.heroes=[];
 
 
 
@@ -30,6 +31,18 @@ class TeamController {
 	}
 
   getData() {
+		this._$http
+		.get(`https://teams.mybluemix.net/api/teams/${this.id}`)
+		.then((response) => {
+			console.log(response);
+			this.team = response.data;
+		});
+
+		this._$http
+		.get(`https://teams.mybluemix.net/api/heroes?filter[where][team_id]=${this.id}`)
+		.then ((response) => {
+			this.heroes = response.data;
+		})
   }
 
 }
